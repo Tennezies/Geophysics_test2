e4ca5bca6f22fde4574d08c2fecdecf08f9ca1e3
Tennezies <ten.atipong@gmail.com>
2025-07-01 11:45:27 +0700

    Create JEEN.py

---
+++ JEEN.py
@@ -0,0 +1,23 @@
+from obspy import UTCDateTime
+from obspy.clients.fdsn import Client
+
+#Download seismic data
+network = "IU"
+station = "TATO"
+channel = "BH*"
+starttime = UTCDateTime("2024-07-22T23:58:11")
+endtime = starttime + 60*15 #15mins
+
+#Client
+client = Client("IRIS")
+
+#Get waveforms
+st = client.get_waveforms(network=network, station=starttime, location ="00", channel=channel, starttime=starttime, endtime=endtime, attach_response=True)
+
+#Convert to velocity
+st.remove_response(output="VEL")
+
+#Pre-processing
+st.merge(fill_value="interpolate") # Merge traces if we have gaps
+st.detrend(type='linear') # Remove linear trend
+st.taper()
